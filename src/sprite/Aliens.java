06d740a4bec48cbd49cc62206f820a6867a57fd5
Mehmethan Bellek <bellekmehmethan@gmail.com>
2023-05-11 23:42:30 +0300

    May 11th, 2023

--- src/sprite/Aliens.java
+++ src/sprite/Aliens.java
@@ -1,4 +1,54 @@
 package sprite;
 
-public class Aliens {
+import javax.swing.ImageIcon;
+
+public class Alien extends Sprite {
+    private Bomb bomb;
+
+    public Alien(int x, int y) {
+        this.initAlien(x, y);
+    }
+
+    private void initAlien(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.bomb = new Bomb(x, y);
+        String alienImg = "src/images/alien.png";
+        ImageIcon img = new ImageIcon(alienImg);
+        this.setImage(img.getImage());
+    }
+
+    public void act(int direction) {
+        this.x += direction;
+    }
+
+    public Bomb getBomb() {
+        return this.bomb;
+    }
+
+    public class Bomb extends Sprite {
+        private boolean destroyed;
+
+        public Bomb(int x, int y) {
+            this.initBomb(x, y);
+        }
+
+        private void initBomb(int x, int y) {
+            this.setDestroyed(true);
+            this.x = x;
+            this.y = y;
+            String bombImg = "src/images/bomb.png";
+            ImageIcon img = new ImageIcon(bombImg);
+            this.setImage(img.getImage());
+        }
+
+        public void setDestroyed(boolean destroyed) {
+            this.destroyed = destroyed;
+        }
+
+        public boolean isDestroyed() {
+            return this.destroyed;
+        }
+    }
 }
+
